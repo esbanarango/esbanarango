69ec1233a742ea772839865297bec122152d2136
Esteba Arango <marranoparael31@gmail.com>
2013-02-09 19:45:10 +0100

    Con el cachorro

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,20 @@
 
 $(function() {
 
+  // Background image
+  $.backstretch('./../images/back.jpg');
+
+  $(window).on("backstretch.show", function (e, instance) {
+    $('.sky').fadeIn();
+  });
+
   // Fixes for the rest
   if (!$.browser.webkit) {
     $('.containerc-content').find('.sidebar').css({'top': '-300px','left': '330px','position': 'relative'})
     $('#card').css({'height': '330px'});
   }
   $('.containerc-content').find('.sidebar').show();
+  
   $('ul.contact_information').find('li.heart').on( "hover", function( e ) {
     var text = ($(this).find('span').text() == 'Animals!')? 'Vegetarian!':'Animals!'
     var t = this
@@ -15,4 +23,5 @@ $(function() {
       $(t).find('span').slideToggle('fast')
     });
   });
+
 });
