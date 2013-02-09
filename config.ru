fcc1390b2d1da3171bfb04649b01114e2692ac7a
Esteba Arango <marranoparael31@gmail.com>
2013-02-09 05:38:21 +0100

    More time

--- config.ru
+++ config.ru
@@ -1,5 +1,5 @@
 use Rack::Static, 
-  :urls => ["/images", "/js", "/css"],
+  :urls => ["/images", "/js", "/css", "/favicon.ico"],
   :root => "public"
 
 run lambda { |env|
--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,18 @@
 
+$(function() {
+
+  // Fixes for the rest
+  if (!$.browser.webkit) {
+    $('.containerc-content').find('.sidebar').css({'top': '-300px','left': '330px','position': 'relative'})
+    $('#card').css({'height': '330px'});
+  }
+  $('.containerc-content').find('.sidebar').show();
+  $('ul.contact_information').find('li.heart').on( "hover", function( e ) {
+    var text = ($(this).find('span').text() == 'Animals!')? 'Vegetarian!':'Animals!'
+    var t = this
+    $(t).find('span').slideToggle('fast',function() {
+      $(t).find('span').text(text);
+      $(t).find('span').slideToggle('fast')
+    });
+  });
+});
