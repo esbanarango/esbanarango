a0dd56da06137641ad3c3a411975354fd3cc61c0
Esteba Arango <marranoparael31@gmail.com>
2013-08-14 00:52:55 -0500

    Fixing styles.

--- public/js/main.js
+++ public/js/main.js
@@ -8,12 +8,6 @@ $(function() {
     $('.sky').css('display','block');
     $('.messages').addClass('animated bounceIn');
   });
-
-  // Fixes for the rest
-  if (!bowser.chrome) {
-    $('.containerc-content').find('.sidebar').css({'top': '-300px','left': '330px','position': 'relative'})
-    $('#card').css({'height': '330px'});
-  }
   $('.containerc-content').find('.sidebar').show();
   
   $('ul.contact_information').find('li.heart').on( "hover", function( e ) {
